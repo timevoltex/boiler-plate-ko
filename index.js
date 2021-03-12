46cd67f3b409b9be1ba7310e4aac4134ff960de2
timevortex <52909529+timevoltex@users.noreply.github.com>
2021-03-12 23:00:16 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
+const bodyParser = require("body-parser");
 const express = require("express");
+const config = require("./config/key");
 
 const app = express();
 const port = 5000;
 
 const mongoose = require("mongoose");
 
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
 mongoose
-  .connect(
-    "mongodb+srv://minerba:413510d8@boilerplate.jybxi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("mongo db connect well"))
   .catch((err) => console.log(err));
 // mongodb+srv://minerba:<password>@boilerplate.jybxi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
-app.get("/", (req, res) => res.send("Hello, World, 안녕하세요"));
+app.get("/", (req, res) => res.send("새해복 많이 받으세요ggg"));
+
+app.post("/register", (req, res) => {
+  //회원가입시 필요한 정보를 client 에서 가져오면
+  //그것들을 DB에 넣어준다.
+
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
